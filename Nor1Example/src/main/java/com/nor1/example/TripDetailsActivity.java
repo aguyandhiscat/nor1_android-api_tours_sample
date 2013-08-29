7709baf67267776abd69865885509ab7bfa9eef9
Alex Wilczewski <alex.wilczewski@nor1.com>
2013-08-29 16:18:08 -0700

    Schedule details are filled in.

--- Nor1Example/src/main/java/com/nor1/example/TripDetailsActivity.java
+++ Nor1Example/src/main/java/com/nor1/example/TripDetailsActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.nor1.example.containers.ImageItem;
+import com.nor1.example.containers.ScheduleItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,6 +27,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -81,7 +83,7 @@ public class TripDetailsActivity extends Activity {
             setUpMediaList(mediaView, timeline.getJSONArray("media"));
 
             ViewGroup schedulesView = (LinearLayout) findViewById(R.id.detail_schedules_list);
-            setUpSchedulesList(schedulesView, timeline.getJSONObject("detail_schedules_list"));
+            setUpSchedulesList(schedulesView, timeline.getJSONObject("schedules"));
         } catch(JSONException e) {
             Log.e(TAG, "Json exception", e);
         }
@@ -126,35 +128,46 @@ public class TripDetailsActivity extends Activity {
 
     protected void setUpSchedulesList(ViewGroup schedulesView, JSONObject schedules) {
         int len = schedules.length();
-        List<ImageItem> list = new ArrayList<ImageItem>();
+        Iterator<String> itr = schedules.keys();
+        List<ScheduleItem> list = new ArrayList<ScheduleItem>();
 
-//        for(int i=0; i<len; i++) {
-//            try {
-//                JSONObject imageData = media.getJSONObject(i);
-//                String description = imageData.getString("description");
-//                JSONArray imageVersions = imageData.getJSONArray("versions");
-//                if(imageVersions.length() > 0) {
-//                    String imageUrl = imageVersions.getJSONObject(0).getString("url");
-//
-//                    View viewElem = View.inflate(this, R.layout.list_item_media, null);
-//                    TextView title = (TextView) viewElem.findViewById(R.id.item_media_title);
-//                    title.setText(description);
-//                    ImageView thumbnail = (ImageView) viewElem.findViewById(R.id.item_media_image);
-//                    thumbnail.setTag(imageUrl);
-//                    new DownloadImageTask().execute(thumbnail);
-//
-//                    mediaView.addView(viewElem);
-//
-//                    ImageItem ii = new ImageItem();
-//                    ii.imageTitle = description;
-//                    ii.imageUrl = imageUrl;
-//
-//                    list.add(ii);
-//                }
-//            } catch(JSONException e) {
-//                Log.e(TAG, "JSON Exception", e);
-//            }
-//        }
+        while(itr.hasNext()) {
+            String key = itr.next();
+
+            View viewElem = View.inflate(this, R.layout.list_item_schedule, null);
+
+            try {
+                JSONObject schedule = schedules.getJSONObject(key);
+
+                ((TextView) viewElem.findViewById(R.id.item_schedule_title)).setText(key);
+                ((TextView) viewElem.findViewById(R.id.item_schedule_title)).setText("Title: "+schedule.getString("title"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_type)).setText("Type: "+schedule.getString("type"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_description)).setText("Description: "+schedule.getString("description"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_commentary)).setText("Commentary: "+schedule.getString("commentary"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_from_date)).setText("From: "+schedule.getString("from"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_to_date)).setText("To: "+schedule.getString("to"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_day_hash)).setText("Day Hash: "+schedule.getString("day_hash"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_first_service)).setText("First Service: "+schedule.getString("first_service"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_last_service)).setText("Last Service: "+schedule.getString("last_service"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_interval)).setText("Interval: "+schedule.getString("interval"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_departure_point)).setText("Departure Point: "+schedule.getString("departure_point"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_time)).setText("Time: "+schedule.getString("time"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_duration)).setText("Duration: "+schedule.getString("duration"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_duration_minutes)).setText("Duration Minutes: "+schedule.getString("duration_minutes"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_frequency)).setText("Frequency: "+schedule.getString("frequency"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_pickup_required)).setText("Pickup Required: "+schedule.getString("pickup_required"));
+                ((TextView) viewElem.findViewById(R.id.item_schedule_dropoff_required)).setText("Dropoff Required: "+schedule.getString("dropoff_required"));
+    //            ((TextView) viewElem.findViewById(R.id.item_schedule_child_price)).setText(schedule.getString("to"));
+    //            ((TextView) viewElem.findViewById(R.id.item_schedule_adult_price)).setText(schedule.getString("to"));
+
+                schedulesView.addView(viewElem);
+
+                ScheduleItem item = new ScheduleItem();
+                list.add(item);
+            } catch(JSONException e) {
+                Log.e(TAG, "JSON Exception", e);
+            }
+        }
     }
 
     public class DownloadImageTask extends AsyncTask<ImageView, Void, Bitmap> {
